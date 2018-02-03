c57308759055a249e107b45abaa5e1e1477952e1
Zygy93 <amazinzygy@gmail.com>
2018-02-03 07:58:35 -0600

    added basic input

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 console.log( 'js' );
 
 $( document ).ready( function(){
-console.log( 'JQ' );
-}); //end doc ready
+  $( '#clickyButton' ).on( 'click', function(){
+    console.log( 'in clickyButton on click' );
+    // capture user input
+    // create an object with the user input
+    var newEmployee = {
+      firstName: $( '#firstIn' ).val(),
+      lastName: $( '#lastIn' ).val(),
+      employeID: $( '#idIn' ).val()
+      jobTitle: $( '#titleIn' ).val(),
+      annualSalary: $( '#salaryIn' ).val(),
+    }
+    // push that information into our list
+    garage.push( newCar );
+    // display what is in our list
+    infoOut();
+    // empty inputs
+    $( '#firstIn' ).val( '' );
+    $( '#lastIn' ).val( '' );
+    $( '#idIn' ).val( '' );
+    $( '#titleIn' ).val( '' );
+    $( '#salaryIn' ).val( '' );
+  });
