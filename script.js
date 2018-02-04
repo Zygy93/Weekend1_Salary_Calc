b6ac19d6b4ddbd644149a6ecc3cc36c30d9cb66a
Zygy93 <amazinzygy@gmail.com>
2018-02-04 16:35:07 -0600

    everything but the monthly display

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-console.log( 'js' );
-
+var info = [];
+var monthlyUpkeep = [];
 $( document ).ready( function(){
-  $( '#clickyButton' ).on( 'click', function(){
-    console.log( 'in clickyButton on click' );
+  $( '#button' ).on( 'click', function(){
     // capture user input
     // create an object with the user input
     var newEmployee = {
       firstName: $( '#firstIn' ).val(),
       lastName: $( '#lastIn' ).val(),
-      employeID: $( '#idIn' ).val()
+      employeID: $( '#idIn' ).val(),
       jobTitle: $( '#titleIn' ).val(),
       annualSalary: $( '#salaryIn' ).val(),
-    }
+    };
     // push that information into our list
-    garage.push( newCar );
+    info.push( newEmployee );
     // display what is in our list
-    infoOut();
+      displayInfo();
     // empty inputs
     $( '#firstIn' ).val( '' );
     $( '#lastIn' ).val( '' );
@@ -23,3 +22,37 @@ $( document ).ready( function(){
     $( '#titleIn' ).val( '' );
     $( '#salaryIn' ).val( '' );
   });
+  $( '#infoOut' ).on( 'click', '.takeOutButton',function(){
+    console.log( 'in takeOutButton on click', $( this ).data( 'id' ) );
+    var employeeID = $( this ).data( 'id' );
+    var companyEmployee = info.splice( employeeID, 1 );
+    $( '#firstIn' ).val( companyEmployee[ 0 ].firstName );
+    $( '#lastIn' ).val( companyEmployee[ 0 ].lastName );
+    $( '#idIn' ).val( companyEmployee[ 0 ].employeID );
+    $( '#titleIn' ).val( companyEmployee[ 0 ].jobTitle );
+    $( '#salaryIn' ).val( companyEmployee[ 0 ].annualSalary );
+    displayInfo();
+  });
+  }); //end doc ready
+
+function displayInfo(){
+  console.log( 'in infoDisplay' );
+  // target output element by ID
+  var output = $( '#infoOut' );
+
+  // empty output element
+  output.empty();
+  //loop
+  for( var i=0; i<info.length ; i++ ){
+    // append each em into output element
+    var outputString = '<li>';
+    outputString += info[ i ].firstName + ' ';
+    outputString += info[ i ].lastName + ' ';
+    outputString += info[ i ].employeID + ' ';
+    outputString += info[ i ].jobTitle + ' ';
+    outputString += info[ i ].annualSalary;
+    //outputString += info[ i ].upkeepIn;
+    outputString += ' <button class="takeOutButton" data-id="' + i + '">YOU ARE FIRED!</button></li>';
+    output.append( outputString );
+}
+}
